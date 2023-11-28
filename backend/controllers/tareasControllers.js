070b4229b9541f2fc7dc055819a9310d18d3bc22
alfredo.cruz <alfredo.cruz@becasbenitojuarez.gob.mx>
2023-11-27 21:28:12 -0600

    Segunda sesión

--- backend/controllers/tareasControllers.js
+++ backend/controllers/tareasControllers.js
@@ -1,18 +1,30 @@
-const getTareas = (req, res) => {
-    res.status(200).json({ message: "Get task" })
-}
+const asyncHandler = require('express-async-handler')
+const Tarea = require('../model/tareasModel')
 
-const setTareas = (req, res) => {
-    res.status(201).json({ message: "Task created" })
-}
+const getTareas = asyncHandler(async (req, res) => {
+    const tareas = await Tarea.find()
+    res.status(200).json(tareas)
+})
 
-const updateTareas = (req, res) => {
+const setTareas = asyncHandler(async (req, res) => {
+    if (!req.body.texto) {
+        res.status(400)
+        throw new Error("Por favor teclea una descripcion")
+    }
+    const tarea = await Tarea.create({
+        texto: req.body.texto
+    })
+
+    res.status(201).json(tarea)
+})
+
+const updateTareas = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Update task with id ${req.params.id}` })
-}
+})
 
-const deleteTareas = (req, res) => {
+const deleteTareas = asyncHandler(async (req, res) => {
     res.status(204).json({ message: `Deleted task with id ${req.params.id}` })
-}
+})
 
 module.exports = {
     getTareas,
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
+const connectDB = require('./config/db')
+const { errorHandler } = require('./middleware/errorMiddleware')
+const colors = require('colors')
+
+connectDB()
 
 const app = express()
 
 app.use('/api/tareas', require('./routes/tareasRoutes'))
+
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server start in port ${port}`))
