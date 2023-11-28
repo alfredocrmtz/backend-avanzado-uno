e6271882bf03e244000de27eef26b2280bd8abea
alfredo.cruz <alfredo.cruz@becasbenitojuarez.gob.mx>
2023-11-28 09:39:08 -0600

    Corrección Body en server.js

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,9 @@ connectDB()
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 app.use('/api/tareas', require('./routes/tareasRoutes'))
 
 app.use(errorHandler)
